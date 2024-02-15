01c9514394d6ccfe75200832f8840c765437ddb1
Edgar Duc <edgar.duc@eleves.enpc.fr>
2024-02-15 17:03:37 +0100

    Correctifs mineurs

--- Logiciel_PEP/.venv/Scripts/activate.bat
+++ Logiciel_PEP/.venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\adles\OneDrive\Bureau\PROJET_TDLOG\Logiciel_PEP\.venv
+set VIRTUAL_ENV=C:\Users\Edgar\Documents\Ponts 2A\Logiciel PEP\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
@@ -25,7 +25,6 @@ if defined _OLD_VIRTUAL_PATH set PATH=%_OLD_VIRTUAL_PATH%
 if not defined _OLD_VIRTUAL_PATH set _OLD_VIRTUAL_PATH=%PATH%
 
 set PATH=%VIRTUAL_ENV%\Scripts;%PATH%
-set VIRTUAL_ENV_PROMPT=(.venv) 
 
 :END
 if defined _OLD_CODEPAGE (
