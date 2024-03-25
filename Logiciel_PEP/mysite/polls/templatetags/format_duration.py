65a46b5cd08698c1b9a6578d428ca03abd411793
Edgar Duc <edgar.duc@eleves.enpc.fr>
2024-03-25 18:51:52 +0100

    Page details qui commence à claquer

--- Logiciel_PEP/mysite/polls/templatetags/format_duration.py
+++ Logiciel_PEP/mysite/polls/templatetags/format_duration.py
@@ -1,5 +1,7 @@
 from django import template
 from django.utils import timesince
+from django.db.models import Sum, F
+from polls.models import AssignationJEH
 register = template.Library()
 
 @register.filter(name='format_duration')
@@ -16,3 +18,14 @@ def format_duration(value):
         # Number of days
         days = int(value.total_seconds() // 86400)
         return f"{days} days"
+    
+@register.filter(name='assignation')
+def assignation(eleve, etude):
+    return list(AssignationJEH.objects.filter(eleve=eleve, phase__etude=etude))
+
+@register.filter(name='JEH')
+def JEH(eleve, etude):
+    queryset = AssignationJEH.objects.filter(eleve=eleve, phase__etude=etude)
+    nb_total_JEH = queryset.aggregate(total_sum=Sum('nombre_JEH'))['total_sum']
+    montant_total = sum(obj.retribution_brute_totale for obj in queryset)
+    return str(nb_total_JEH)+" JEH pour un montant de "+str(montant_total)+"€ HT"
