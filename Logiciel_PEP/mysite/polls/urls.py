f4dd3b14f23600b354132cfcdfbec7790080509b
Edgar Duc <edgar.duc@eleves.enpc.fr>
2024-03-27 15:24:54 +0100

    Informations modifiables sur l'onglet principal Etude

--- Logiciel_PEP/mysite/polls/urls.py
+++ Logiciel_PEP/mysite/polls/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("ajouter_assignation_jeh/<int:id_etude>/<int:numero_phase>", views.ajouter_assignation_jeh, name='ajouter_assignation_jeh'),
     path("BV/<int:id_etude>", views.BV, name='BV'),
     path("ndf/", views.ndf, name="ndf"),
-    path("recrutement/<str:id_url>", views.recrutement, name="recrutement")
+    path("recrutement/<str:id_url>", views.recrutement, name="recrutement"),
+    path("modifier_recrutement_etude/<int:iD>", views.modifier_recrutement_etude, name="modifier_recrutement_etude"),
     
 ]
