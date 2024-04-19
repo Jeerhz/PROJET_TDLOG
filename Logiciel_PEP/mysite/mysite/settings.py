18b960a857335b131b165de2c6644d505a2d5248
Jeerhz <adlesalem@hotmail.fr>
2024-04-19 14:20:51 +0200

    debug csrf authentification

--- Logiciel_PEP/mysite/mysite/settings.py
+++ Logiciel_PEP/mysite/mysite/settings.py
@@ -80,6 +80,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "CsrfViewMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "csp.middleware.CSPMiddleware",
 ]
