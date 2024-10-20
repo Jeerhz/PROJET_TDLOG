3d7d95b3e656db64e0480e15f06e3356832ea512
Antony Feord <antonyfeord21@gmail.com>
2024-10-20 11:12:09 +0200

    pull

--- Logiciel_PEP/mysite/mysite/settings.py
+++ Logiciel_PEP/mysite/mysite/settings.py
@@ -82,9 +82,9 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_sylex',
-        'USER': 'edgar_admin',
-        'PASSWORD': '/Tito1905',
+        'NAME': 'db_sylog2',
+        'USER': 'postgres',
+        'PASSWORD': 'sylog',
         'HOST': 'localhost',  
         'PORT': '5432',
     }
