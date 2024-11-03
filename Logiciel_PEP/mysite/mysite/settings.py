11b593fb86285eaa318632c8cc2dd0dce81e5226
Jeerhz <adlesalem@hotmail.fr>
2024-11-03 15:12:31 +0100

    réparation register et ajout modifier phase

--- Logiciel_PEP/mysite/mysite/settings.py
+++ Logiciel_PEP/mysite/mysite/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_sylog2',
+        'NAME': 'db_sylog',
         'USER': 'postgres',
-        'PASSWORD': 'sylog',
+        'PASSWORD': 'bensalem',
         'HOST': 'localhost',  
-        'PORT': '5432',
+        'PORT': '5433',
     }
 }
 
