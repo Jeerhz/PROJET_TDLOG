5f8b1efff18b7ae1827187aca88d60317ff1ce66
Jeerhz <adlesalem@hotmail.fr>
2024-11-24 20:49:11 +0100

    add debug toolbar to installed apps

--- Logiciel_PEP/mysite/mysite/settings.py
+++ Logiciel_PEP/mysite/mysite/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "django_celery_beat",
     "django_celery_results",
     "social_django",
