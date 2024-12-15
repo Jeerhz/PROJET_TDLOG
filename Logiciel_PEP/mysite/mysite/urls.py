93e1d22fda1ce85e3c9213d6c3a9a721defb594e
Jeerhz <adlesalem@hotmail.fr>
2024-12-15 22:26:10 +0100

    fixing major issue commented a bracked

--- Logiciel_PEP/mysite/mysite/urls.py
+++ Logiciel_PEP/mysite/mysite/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),
     path("auth/", include("social_django.urls", namespace="social")),
-# ]  + debug_toolbar_urls()
+]  # + debug_toolbar_urls()
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
