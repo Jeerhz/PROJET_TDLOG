ab942da32c7b491bdc0ab98bc0c2467ef58fb4cf
Antony Feord <antonyfeord21@gmail.com>
2025-03-19 08:38:16 +0100

    nouveaux docs

--- Logiciel_PEP/mysite/polls/models.py
+++ Logiciel_PEP/mysite/polls/models.py
@@ -772,7 +772,7 @@ class Etude(models.Model):
     )
 
     paragraphe_intervenant_devis = models.TextField(
-        default="Pour réaliser votre étude, nous rechercherons un ou des étudiants de l’École des Ponts ParisTech. Les cours dispensés à l’École tel(s) que [exemples(s) de cours qui peuvent être utile pour réaliser la mission], apportent aux étudiants les outils nécessaires pour [ce en quoi l'étude va consister]. Ils auront donc les connaissances requises pour [ce que veut le client]."
+        default="Pour réaliser votre étude, nous rechercherons un ou des étudiants de l’École Nationale des Ponts et Chaussées. Les cours dispensés à l’École tel(s) que [exemples(s) de cours qui peuvent être utile pour réaliser la mission], apportent aux étudiants les outils nécessaires pour [ce en quoi l'étude va consister]. Ils auront donc les connaissances requises pour [ce que veut le client]."
     )
     cahier_des_charges = models.JSONField(default=dict)
     date_creation = models.DateTimeField(auto_now_add=True)
