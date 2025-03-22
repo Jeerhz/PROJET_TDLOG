f00c83b1e5698714187e895419edc144e3369f78
Jeerhz <adlesalem@hotmail.fr>
2025-03-22 16:46:21 +0100

    Refactor error page and update settings for improved clarity and consistency

--- Logiciel_PEP/mysite/mysite/settings.py
+++ Logiciel_PEP/mysite/mysite/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "debug_toolbar",#TODO
+    "debug_toolbar",  # TODO
     "django_celery_beat",
     "social_django",
 ]
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",#TODO
+    "debug_toolbar.middleware.DebugToolbarMiddleware",  # TODO
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
@@ -83,13 +83,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_sylog2',
-        'USER': 'postgres',
-        'PASSWORD': 'sylog',
-        'HOST': 'localhost',  
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "db_sylog",
+        "USER": "postgres",
+        "PASSWORD": "bensalem",
+        "HOST": "localhost",
+        "PORT": "5433",
     }
 }
 
@@ -113,25 +113,27 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 AUTHENTICATION_BACKENDS = (
-    'social_core.backends.google.GoogleOAuth2',
-    'django.contrib.auth.backends.ModelBackend',
+    "social_core.backends.google.GoogleOAuth2",
+    "django.contrib.auth.backends.ModelBackend",
     # other backends
 )
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = "162876035246-14ukf5eahlqccekrf1sv4lm7npb77vs3.apps.googleusercontent.com"
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = (
+    "162876035246-14ukf5eahlqccekrf1sv4lm7npb77vs3.apps.googleusercontent.com"
+)
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = "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"
 SOCIAL_AUTH_GOOGLE_OAUTH2_SCOPE = [
-    'https://www.googleapis.com/auth/userinfo.email',
-    'https://www.googleapis.com/auth/userinfo.profile',
-    'https://www.googleapis.com/auth/gmail.send',
-    'openid',
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/userinfo.profile",
+    "https://www.googleapis.com/auth/gmail.send",
+    "openid",
 ]
 SOCIAL_AUTH_GOOGLE_OAUTH2_AUTH_EXTRA_ARGUMENTS = {
-    'access_type': 'offline',
-    'prompt': 'consent',  # Force Google to ask the user to re-consent
+    "access_type": "offline",
+    "prompt": "consent",  # Force Google to ask the user to re-consent
 }
-SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ['email', 'name', 'refresh_token', 'expires_in']
-LOGIN_REDIRECT_URL = '/polls/google-login'
+SOCIAL_AUTH_GOOGLE_OAUTH2_EXTRA_DATA = ["email", "name", "refresh_token", "expires_in"]
+LOGIN_REDIRECT_URL = "/polls/google-login"
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -154,7 +156,7 @@ DATETIME_FORMAT = "d/m/Y H:i:s"
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "polls/static")
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
 
 
@@ -165,8 +167,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 CELERY_BEAT_SCHEDULE = {
-    'send-reminder-emails-every-day': {
-        'task': 'polls.tasks.send_reminder_emails',
-        'schedule': crontab(hour=8, minute=0),  # Adjust this according to your needs
+    "send-reminder-emails-every-day": {
+        "task": "polls.tasks.send_reminder_emails",
+        "schedule": crontab(hour=8, minute=0),  # Adjust this according to your needs
     },
 }
